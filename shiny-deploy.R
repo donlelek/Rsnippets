6f398a11c935b63cde6047fbc3ead78e0197a021
Derek Price <dprice@upei.ca>
2015-01-12 16:25:43 -0400

    added shiny deploy code

---
+++ shiny-deploy.R
@@ -0,0 +1,13 @@
+setwd("/Users/dprice/Documents/GIT/shiny-diamonds-demo")
+
+# test app
+library(shiny)
+runApp()
+
+# deploy app
+library(shinyapps)
+shinyapps::deployApp()
+
+# deployed to:
+# https://dprice.shinyapps.io/shiny-diamonds-demo/
+
