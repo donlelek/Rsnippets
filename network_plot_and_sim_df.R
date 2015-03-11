1b0c31e39f4a5ddc8933d6cc3b75b0c4f45ac470
Derek Price <dprice@upei.ca>
2015-03-10 23:58:26 -0300

    added network plotting

---
+++ network_plot_and_sim_df.R
@@ -0,0 +1,23 @@
+library("igraph")
+
+# simulate dataset with three columns: from, to, weight
+set.seed(1828)
+mov <- data.frame(from = sample(LETTERS[1:10], 50, replace = TRUE, prob = runif(10)),
+                  to = sample(LETTERS[1:10], 50, replace = TRUE, prob = runif(10)),
+                  weight = 30*runif(50))
+
+# Load (DIRECTED) graph from data frame 
+g <- graph.data.frame(mov[1:3], directed=TRUE)
+
+# Plot graph
+plot(g,
+     vertex.shape = "circle",
+     vertex.frame.color = "SkyBlue2",
+     vertex.size = g[3],
+     vertex.label.cex = 0.8,
+     edge.curved = TRUE,
+     edge.arrow.size = 0.3)
+
+# 3D plot
+rglplot(g,
+        vertex.size = g[3]) 
