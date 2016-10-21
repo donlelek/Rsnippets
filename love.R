46eca5bb3f31794bba77807ef5b74ff929a6ddfe
donlelek <dpricer@gmail.com>
2016-10-21 16:22:35 -0300

    LOVE!

--- love.R
+++ love.R
@@ -14,26 +14,32 @@ V <- data_frame(x = -50:50,
                 y = abs(-2 * x))
 E <- data_frame(y = seq(-pi, pi, l = 100),
                 x = -3 * abs(sin(y)))
-pd <- bind_rows(L = L, O = O, V = V, E = E, .id = 'letter')
-
-pd$letter <- factor(pd$letter, 
-                    c('L', 'O', 'V', 'E'),
-                    c('y == 1/x', 'x^2 + y^2 == 9', 'y == abs(-2*x)', 'x == -3*abs(sin(y))'))
+pd <- bind_rows(L, O, V, E, .id = "letter") %>%
+  select(-t) %>% 
+  mutate(letter = factor(letter, labels = c("y == 1/x",
+                                            "x^2 + y^2 == 9",
+                                            "y == abs(-2*x)",
+                                            "x == -3*abs(sin(y))")))
 
 ggplot(pd, aes(x, y)) +
   geom_vline(xintercept = 0, color = "white") + 
   geom_hline(yintercept = 0, color = "white") +
-  geom_path(size = 1.5, col = 'red') +
-  facet_wrap(~letter, scales = 'free', labeller = label_parsed, nrow = 1, switch = "x") +
+  geom_path(size = 1.5, color = "red") +
+  facet_wrap(~letter,
+             scales = "free",
+             labeller = label_parsed,
+             nrow = 1,
+             switch = "x") +
+  labs(title = "ALL YOU NEED IS", caption = "...and ggplot") +
   theme_minimal() +
-  ggtitle("ALL YOU NEED IS") +
-  theme(text = element_text(size = 30),
+  theme(text = element_text(color = "white"),
         plot.background = element_rect(fill = "black", color = "black"),
         axis.text = element_blank(), 
         axis.title = element_blank(),
         panel.grid = element_blank(),
-        strip.text = element_text(color = "white", face = "italic"),
-        title = element_text(color = "white", face = "bold"))
+        strip.text = element_text(color = "white", size = 50),
+        plot.title = element_text(face = "bold", size = 50),
+        plot.caption = element_text(color = "white", size = 20))
 
 ggsave("~/Downloads/love_android.png", width = 19.20, height = 10.80, dpi = 100)
  
